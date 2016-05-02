d035c4e5a1022a81ea64f081cdd53c6c261dedc9
Zbigniew Reszela <zreszela@cells.es>
2016-05-02 16:45:22 +0200

    Add validation of zone_plate positions

--- manytomos.py
+++ manytomos.py
@@ -10,6 +10,8 @@ regions_def = [['start', Type.Float, None, 'Theta start position'],
                ['theta_step', Type.Integer, 1, 'Theta step'],
                 {'min' : 1, 'max' : 3 }]
 
+NAME = 0 # name position in sample
+ZP_Z = 4 # ZP_Z position in sample
 
 class manytomos(Macro):
     """Generates TXM input file with commands to perform multi-sample tomo
@@ -21,16 +23,28 @@ class manytomos(Macro):
                      ['pos_y', Type.Float, None, 'Position of the Y motor'],
                      ['pos_z', Type.Float, None, 'Position of the Z motor'],
                      ['ZP_Z', zone_plate_def, None, ('Zone plates'
-                        'positions')],
-                     ['sample_theta', regions_def, None, 'Regions of the theta motor'],
+                         ' positions')],
+                     ['sample_theta', regions_def, None, ('Regions of the'
+                         ' theta motor'],
                      ['ff_pos_x', Type.Float, None, ('Position of the X motor'
-                        ' for the flat field acquisition')],
+                         ' for the flat field acquisition')],
                      ['ff_pos_y', Type.Float, None, ('Position of the Y motor'
-                        ' for the flat field acquisition')]],
+                         ' for the flat field acquisition')]],
             None, 'List of samples'],
         ['out_file', Type.Filename, None, 'Output file'],
     ]
 
+    def _verifySamples(self, samples):
+        for sample in samples:
+            # check the zone_plate value
+            for counter, zone_plate in enumerate(sample[ZP_Z]):
+                if zone_plate < -11275.0 or zone_plate > -11200.0:
+                    msg = ("The sample {0} has the zone_plate #{1} out of"
+                           " range. The accepted range is from -11275.0 to"
+                           " -11200.0 um.")
+                    raise ValueError(msg.format(sample[NAME], counter))
+
     def run(self, samples, filename):
+        self._verifySamples(samples)
         manytomos = ManyTomos(samples, filename)
         manytomos.generate()
