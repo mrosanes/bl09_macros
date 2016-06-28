3f725896ad4fe0e0975a99d596ea275ef6b0eaf4
cfalcon <cfalcon@cells.es>
2016-06-28 11:29:13 +0200

    Add manytomosAvg macro
    This is an user request: RT#62910
    
    Fix verifySamples message.

--- manytomos.py
+++ manytomos.py
@@ -13,7 +13,7 @@ regions_def = [['start', Type.Float, None, 'Theta start position'],
                ['end', Type.Float, None, 'Theta end position'],
                ['exp_time', Type.Float, None, 'Exposure time'],
                ['theta_step', Type.Integer, 1, 'Theta step'],
-                {'min' : 1, 'max' : 3 }]
+                {'min' : 1, 'max' : 5 }]
 
 NAME = 0 # name position in sample
 ZP_Z = 4 # ZP_Z position in sample
@@ -29,8 +29,8 @@ class manytomosbase:
             for counter, zone_plate in enumerate(sample[ZP_Z]):
                 if zone_plate < zp_limit_neg or zone_plate > zp_limit_pos:
                     msg = ("The sample {0} has the zone_plate #{1} out of"
-                           " range. The accepted range is from -11275.0 to"
-                           " -11200.0 um.")
+                           " range. The accepted range is from %s to"
+                           " %s um.") % (zp_limit_neg, zp_limit_pos)
                     raise ValueError(msg.format(sample[NAME], counter))
 
     def run(self, samples, filename):
@@ -66,6 +66,25 @@ class manytomos(manytomosbase, Macro):
         ['out_file', Type.Filename, None, 'Output file'],
     ]
 
+class manytomosAvg(manytomosbase, Macro):
+
+    param_def = [
+        ['samples', [['name', Type.String, None, 'Sample name'],
+                      ['pos_x', Type.Float, None, 'Position of the X motor'],
+                     ['pos_y', Type.Float, None, 'Position of the Y motor'],
+                     ['pos_z', Type.Float, None, 'Position of the Z motor'],
+                     ['ZP_Z', zone_plate_def, None, ('Zone plates'
+                         ' positions')],
+                     ['sample_theta', regions_def, None, ('Regions of the'
+                         ' theta motor')],
+                     ['ff_pos_x', Type.Float, None, ('Position of the X motor'
+                         ' for the flat field acquisition')],
+                     ['ff_pos_y', Type.Float, None, ('Position of the Y motor'
+                         ' for the flat field acquisition')],
+                    ['n_images', Type.Integer, 1, 'Number of images per saple']],
+            None, 'List of samples'],
+        ['out_file', Type.Filename, None, 'Output file'],
+    ]
 
 class manytomosE(manytomosbase, Macro):
 
