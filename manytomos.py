284493fb78d9f54384280d2789a8f27f90e60ffc
mrosanes <mrosanes@cells.es>
2017-04-07 12:53:31 +0200

    Reorganize manytomos macro

--- manytomos.py
+++ manytomos.py
@@ -1,48 +1,48 @@
-from manytomos import ManyTomos
 from sardana.macroserver.macro import Macro, Type
 from sardana.macroserver.msexception import UnknownEnv
+from collectlib.tomoslib import ManyTomos
 
-zone_plate_def = [['ZP_central_pos', Type.Float, None, ('Position of the zone'
-                    ' plate motor')],
-                  ['ZP_step', Type.Float, None, 'Zone plate z step'],
-                  {'min' : 1, 'max' : 1}]
-
-energy_def = [['ZP_central_pos', Type.Float, None, 'Zone plate central '
-               'position'],
-              ['ZP_step', Type.Float, None, 'Zone plate z step'],
-              ['energy', Type.Float, None, 'Beam energy'],
-              {'min' : 1}]
+energy_zp_def = [['energy', Type.Float, None, 'Beam energy'],
+                 ['det_z', Type.Float, None, 'Detector Z position'],
+                 ['zp_z', Type.Float, None, 'ZonePlate Z position'],
+                 ['zp_step', Type.Float, None, 'ZonePlate step'],
+                 {'min': 1}]
 
 regions_def = [['start', Type.Float, None, 'Theta start position'],
                ['end', Type.Float, None, 'Theta end position'],
-               ['exp_time', Type.Float, None, 'Exposure time'],
                ['theta_step', Type.Integer, 1, 'Theta step'],
-                {'min' : 1, 'max' : 5 }]
+               ['exp_time', Type.Float, None, 'Exposure time'],
+               {'min': 1, 'max': 10}]
 
-NAME = 0 # name position in sample
-ZP_Z = 4 # ZP_Z position in sample
+# name position in sample
+NAME = 0
 
-class manytomosbase:
+# energy_zp position in the macro parameters
+E_ZP_ZONES = 4
+
+class manytomosbase(object):
     """Generates TXM input file with commands to perform multi-sample tomo
     measurements.
     """
 
-    def _verifySamples(self, samples, zp_limit_neg, zp_limit_pos):
+    def _verify_samples(self, samples, zp_limit_neg, zp_limit_pos):
         for sample in samples:
-            for counter, zone_plate in enumerate(sample[ZP_Z]):
-                zp_central_pos = zone_plate[0]
-                zp_step = zone_plate[1]
-                zp_pos_1 = zp_central_pos - zp_step
-                zp_pos_2 = zp_central_pos
-                zp_pos_3 = zp_central_pos + zp_step
-                zp_positions = [zp_pos_1, zp_pos_2, zp_pos_3] 
+            for counter, e_zp_zone in enumerate(sample[E_ZP_ZONES]):
+                zp_central_pos = e_zp_zone[2]
+                zp_step = e_zp_zone[3]
+                if zp_step == 0:
+                    zp_positions = [zp_central_pos]
+                else:
+                    zp_pos_1 = zp_central_pos - zp_step
+                    zp_pos_2 = zp_central_pos
+                    zp_pos_3 = zp_central_pos + zp_step
+                    zp_positions = [zp_pos_1, zp_pos_2, zp_pos_3]
                 for zp_position in zp_positions:
-                    if (zp_position < zp_limit_neg or 
-                        zp_position > zp_limit_pos):
-                        msg = ("The sample {0} has the zone_plate #{1} out of"
-                           " range. The accepted range is from %s to"
-                           " %s um.") % (zp_limit_neg, zp_limit_pos)
-                        raise ValueError(msg.format(sample[NAME], counter+1))                
+                    if zp_position < zp_limit_neg or zp_position > zp_limit_pos:
+                        msg = ("The sample {0} has the zone_plate {1} out of"
+                               " range. The accepted range is from %s to"
+                               " %s um.") % (zp_limit_neg, zp_limit_pos)
+                        raise ValueError(msg.format(sample[NAME], counter+1))
 
     def run(self, samples, filename):
         try:
@@ -53,9 +53,9 @@ class manytomosbase:
             zp_limit_pos = self.getEnv("ZP_Z_limit_pos")
         except UnknownEnv:
             zp_limit_pos = float("Inf")
-        self._verifySamples(samples, zp_limit_neg, zp_limit_pos)
-        manytomos = ManyTomos(samples, filename)
-        manytomos.generate()
+        self._verify_samples(samples, zp_limit_neg, zp_limit_pos)
+        tomos_obj = ManyTomos(samples, filename)
+        tomos_obj.generate()
 
 
 class manytomos(manytomosbase, Macro):
@@ -65,53 +65,19 @@ class manytomos(manytomosbase, Macro):
                      ['pos_x', Type.Float, None, 'Position of the X motor'],
                      ['pos_y', Type.Float, None, 'Position of the Y motor'],
                      ['pos_z', Type.Float, None, 'Position of the Z motor'],
-                     ['ZP_Z', zone_plate_def, None, ('Zone plates'
-                                                     ' positions')],
+                     ['energy_zp', energy_zp_def, None, 'energy ZP zones'],
                      ['sample_theta', regions_def, None, ('Regions of the'
-                         ' theta motor')],
+                                                          ' theta motor')],
                      ['ff_pos_x', Type.Float, None, ('Position of the X motor'
-                         ' for the flat field acquisition')],
+                                                     ' for the flat field'
+                                                     ' acquisition')],
                      ['ff_pos_y', Type.Float, None, ('Position of the Y motor'
-                         ' for the flat field acquisition')]],
+                                                     ' for the flat field'
+                                                     ' acquisition')],
+                     ['exp_time_ff', Type.Float, None, 'FF exposure time'],
+                     ['n_images', Type.Integer, 1, ('Number of images'
+                                                    ' per angle')]],
             None, 'List of samples'],
         ['out_file', Type.Filename, None, 'Output file'],
     ]
 
-class manytomosAvg(manytomosbase, Macro):
-
-    param_def = [
-        ['samples', [['name', Type.String, None, 'Sample name'],
-                      ['pos_x', Type.Float, None, 'Position of the X motor'],
-                     ['pos_y', Type.Float, None, 'Position of the Y motor'],
-                     ['pos_z', Type.Float, None, 'Position of the Z motor'],
-                     ['ZP_Z', zone_plate_def, None, ('Zone plates'
-                         ' positions')],
-                     ['sample_theta', regions_def, None, ('Regions of the'
-                         ' theta motor')],
-                     ['ff_pos_x', Type.Float, None, ('Position of the X motor'
-                         ' for the flat field acquisition')],
-                     ['ff_pos_y', Type.Float, None, ('Position of the Y motor'
-                         ' for the flat field acquisition')],
-                    ['n_images', Type.Integer, 1, 'Number of images per saple']],
-            None, 'List of samples'],
-        ['out_file', Type.Filename, None, 'Output file'],
-    ]
-
-class manytomosE(manytomosbase, Macro):
-
-    param_def = [
-        ['samples', [['name', Type.String, None, 'Sample name'],
-                     ['pos_x', Type.Float, None, 'Position of the X motor'],
-                     ['pos_y', Type.Float, None, 'Position of the Y motor'],
-                     ['pos_z', Type.Float, None, 'Position of the Z motor'],
-                     ['ZP_Z', energy_def, None, ('Beam energies and ZP_Z'
-                                                 ' positions')],
-                     ['sample_theta', regions_def, None, ('Regions of the'
-                         ' theta motor')],
-                     ['ff_pos_x', Type.Float, None, ('Position of the X motor'
-                         ' for the flat field acquisition')],
-                     ['ff_pos_y', Type.Float, None, ('Position of the Y motor'
-                         ' for the flat field acquisition')]],
-            None, 'List of samples'],
-        ['out_file', Type.Filename, None, 'Output file'],
-    ]
