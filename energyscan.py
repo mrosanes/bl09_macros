2234186d7dab3f6f84e1d64e02c6775a6307a6d7
mrosanes <mrosanes@cells.es>
2017-04-07 12:53:11 +0200

    Reorganize energyscan.py macro

--- energyscan.py
+++ energyscan.py
@@ -1,23 +1,21 @@
-from energyscanlib import EnergyScan
 from sardana.macroserver.macro import Macro, Type
-from sardana.macroserver.msexception import UnknownEnv
-
+from collectlib.energyscanlib import EnergyScan
 
 energy_def = [['E_start', Type.Float, None, 'Energy start position'],
               ['E_end', Type.Float, None, 'Energy end position'],
               ['E_step', Type.Float, None, 'Energy step'],
               ['exp_time', Type.Float, None, 'Sample exposure time'],
               ['exp_time_FF', Type.Float, None, 'FF exposure time'],
-               {'min' : 1, 'max' : 50 }]
+              {'min': 1, 'max': 50 }]
 
 sample_pos_def = [['pos_x', Type.Float, None, 'Position of the X motor'],
                   ['pos_y', Type.Float, None, 'Position of the Y motor'],
                   ['pos_z', Type.Float, None, 'Position of the Z motor'],
-                   {'min' : 1, 'max' : 80 }]
+                  {'min': 1, 'max': 80 }]
 
 
 
-class energyscanbase:
+class energyscanbase(object):
     """Generates TXM input file with commands to perform multi-sample tomo
     measurements.
     """
@@ -32,7 +30,8 @@ class energyscan(energyscanbase, Macro):
     param_def = [
         ['samples', [['name', Type.String, None, 'Sample name'],
                      ['sample_regions',  sample_pos_def, None, ('Regions of the'
-                        ' sample to be imaged')],
+                                                                ' sample to be'
+                                                                ' imaged')],
                      ['energy_regions', energy_def, None, ('Regions of the'
                          ' energy motor')],
                      ['ZP_start', Type.Float, None, 'ZP start position'],
@@ -40,35 +39,16 @@ class energyscan(energyscanbase, Macro):
                      ['Det_start', Type.Float, None, 'Detector start position'],
                      ['Det_end', Type.Float, None, 'Detector end position'],  
                      ['ff_pos_x', Type.Float, None, ('Position of the X motor'
-                         ' for the flat field acquisition')],
+                                                     ' for the flat field'
+                                                     ' acquisition')],
                      ['ff_pos_y', Type.Float, None, ('Position of the Y motor'
-                         ' for the flat field acquisition')],
+                                                     ' for the flat field'
+                                                     ' acquisition')],
                      ['n_images', Type.Integer, 1, ('Number of images per '
                       'energy')]],
             None, 'List of samples'],
         ['out_file', Type.Filename, None, 'Output file name'],
     ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
